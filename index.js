e80ae4535bcefec503502f70dd05426943d518d6
Vineet Katiyar <vineetkatiyar98@gmail.com>
2025-03-15 16:44:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./dbConfig.js";
+import bookRouter from "./routes/bookRoute.js";
+import cors from "cors";
+
+app.use(express.json());
+
+app.use(cors());
+
+connectDB();
+
+app.use("/book", bookRouter);
+
+const port = process.env.PORT || 8000;
+
+app.get("/test", (req, response) => {
+  response.send("Welcome");
+});
+
+app.listen(port, function () {
+  console.log(`listening on ${port}`);
+});
